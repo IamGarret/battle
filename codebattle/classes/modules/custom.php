fdf120a5f85053f776fcedf783766be4ce1bba32
IamGarret <s1.e1@ya.ru>
2017-10-06 14:00:56 +0300

    update custom.php
    ea

--- codebattle/classes/modules/custom.php
+++ codebattle/classes/modules/custom.php
@@ -7,6 +7,10 @@
 		public function __call($method, $args) {
 			throw new publicException("Method " . get_class($this) . "::" . $method . " doesn't exist");
 		}
+		
+		public function test() {
+			
+		}
 		//TODO: Write your own macroses here
 	};
 ?>
